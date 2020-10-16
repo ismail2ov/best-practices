38c2172d160aa31748dec85fadfd1a6ac8533380
Ismail Ahmedov <ismail.ahmedov@build38.com>
2020-10-16 12:55:08 +0200

    Increment cohesion

--- src/main/java/cohesion/DatabaseClient.java
+++ src/main/java/cohesion/DatabaseClient.java
@@ -1,24 +1,59 @@
 package cohesion;
 
+import java.util.UUID;
+
 public class DatabaseClient {
 
+    Database database = new Database();
+    Player player = new Player();
+    Ranking ranking = new Ranking();
+
     public void connectDatabase() {
+        database.connect();
+    }
+
+    public void printAllPlayersInfo() {
+        player.printAllPlayersInfo();
+    }
+
+    public void printSinglePlayerInfo(UUID playerId) {
+        player.printSinglePlayerInfo(playerId);
+    }
+
+    public void printRankings() {
+        ranking.printRankings();
+    }
+
+    public void closeDatabase() {
+        database.close();
+    }
+}
+
+class Database {
+
+    public void connect() {
         //connecting to database.
     }
+
+    public void close() {
+        //closing the database connection.
+    }
+}
+
+class Player {
 
     public void printAllPlayersInfo() {
         //printing all the players information.
     }
 
-    public void printSinglePlayerInfo() {
+    public void printSinglePlayerInfo(UUID playerId) {
         //printing a single player information.
     }
+}
+
+class Ranking {
 
     public void printRankings() {
         //printing the players current rankings.
-    }
-
-    public void closeDatabase() {
-        //closing the database connection.
     }
 }
