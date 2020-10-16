d5d605c194a1ca798621aa0c5c2fda6e7b8d977f
Ismail Ahmedov <ismail.ahmedov@build38.com>
2020-10-16 12:20:14 +0200

    Replace Nested Conditional with Guard Clauses

--- src/main/java/kiss/Checkout.java
+++ src/main/java/kiss/Checkout.java
@@ -13,48 +13,38 @@ public class Checkout {
 
     public boolean validateCart() throws Exception {
 
-        if (cart.getNumProducts() > 0) {
-
-            if (cart.getTotal() >= cart.MIN_PURCHASE_AMOUNT) {
-
-                if (cart.getTotal() < cart.MAX_PURCHASE_AMOUNT) {
-
-                    if (delivery.checkAddress()) {
-
-                        if (delivery.hasCarrier()) {
-
-                            if (payment.hasPaymentMethods()) {
-
-                                if (payment.isCompulsiveBuyer()) {
-
-                                    if (cart.checkStock()) {
-
-                                        return true;
-
-                                    } else {
-                                        throw new Exception("No hay stock");
-                                    }
-                                } else {
-                                    throw new Exception("Are you a compulsive buyer signed");
-                                }
-                            } else {
-                                throw new Exception("No payment methods");
-                            }
-                        } else {
-                            throw new Exception("No carrier");
-                        }
-                    } else {
-                        throw new Exception("Invalid address");
-                    }
-                } else {
-                    throw new Exception("You have exceeded the maximum total allowed for a purchase");
-                }
-            } else {
-                throw new Exception("Does not reach the minimum required for the cart");
-            }
-        } else {
+        if (cart.getNumProducts() <= 0) {
             throw new Exception("The cart is empty");
         }
 
+        if (!(cart.getTotal() >= Cart.MIN_PURCHASE_AMOUNT)) {
+            throw new Exception("Does not reach the minimum required for the cart");
+        }
+
+        if (!(cart.getTotal() < Cart.MAX_PURCHASE_AMOUNT)) {
+            throw new Exception("You have exceeded the maximum total allowed for a purchase");
+        }
+
+        if (!delivery.checkAddress()) {
+            throw new Exception("Invalid address");
+        }
+
+        if (!delivery.hasCarrier()) {
+            throw new Exception("No carrier");
+        }
+
+        if (!payment.hasPaymentMethods()) {
+            throw new Exception("No payment methods");
+        }
+
+        if (!payment.isCompulsiveBuyer()) {
+            throw new Exception("Are you a compulsive buyer signed");
+        }
+
+        if (!cart.checkStock()) {
+            throw new Exception("No hay stock");
+        }
+
+        return true;
     }
 }
